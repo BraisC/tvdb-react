2b892cdec2eb0d927026e80b0515455ced11664d
Brais <braiscao.dev@gmail.com>
2020-08-03 21:29:03 +0200

    Top Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,41 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Styled } from './styled';
 
+const Menu = () => (
+  <Styled.Menu>
+    <li>
+      <NavLink to="/">
+        <span>home</span>
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/popular">
+        <span>popular</span>
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/top">
+        <span>top</span>
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/onair">
+        <span>on air</span>
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/today">
+        <span>today</span>
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/genre">
+        <span>genre</span>
+      </NavLink>
+    </li>
+  </Styled.Menu>
+);
+
 const Header = (props) => {
   const handleClick = () => {
     props.setTheme((theme) => !theme);
@@ -10,7 +45,7 @@ const Header = (props) => {
   return (
     <Styled.Header>
       <Styled.Logo>TVDB</Styled.Logo>
-      <NavLink to="/">Header</NavLink>
+      <Menu />
       <button onClick={handleClick}>Switch Theme</button>
     </Styled.Header>
   );
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -3,10 +3,7 @@ import styled from 'styled-components';
 const Header = styled.header`
   display: flex;
   background: var(--color-primary);
-
-  & a {
-    color: var(--text-color);
-  }
+  align-items: center;
 `;
 
 const Logo = styled.div`
@@ -17,7 +14,37 @@ const Logo = styled.div`
   padding: 1.3rem 5rem;
 `;
 
+const Menu = styled.ul`
+  padding: 0 2rem;
+  display: flex;
+
+  & li {
+    list-style: none;
+  }
+
+  & a {
+    color: var(--text-color);
+    font-size: 1.8rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 1rem 3rem;
+
+    &:hover {
+      color: var(--color-red);
+    }
+
+    & span {
+      padding: 0.2rem 0rem;
+    }
+
+    &.active span {
+      border-bottom: 2px solid var(--color-red);
+    }
+  }
+`;
+
 export const Styled = {
   Header,
   Logo,
+  Menu,
 };
