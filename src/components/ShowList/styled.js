1638ec6e70d95d4e7e3a472ac482722fba95ca08
Brais <braiscao.dev@gmail.com>
2020-08-16 14:52:23 +0200

    📚 Remove minmax as it was doing nothing

--- src/components/ShowList/styled.js
+++ src/components/ShowList/styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const ShowList = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(10rem, 25rem));
+  grid-template-columns: repeat(auto-fit, 25rem);
   justify-content: space-between;
   gap: 4rem;
   padding: 0 2rem; /* so shadows dont get cut */
