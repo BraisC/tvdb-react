54e8024bace267b4a039a5da525d80586c990d80
Brais <braiscao.dev@gmail.com>
2020-08-18 23:39:45 +0200

    🧼 Deleted previous modal

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,3 @@ export { default as ShowListLoader } from './ShowListLoader';
 export { default as Pagination } from './Pagination';
 export { default as Loader } from './Loader';
 export { default as Button } from './Button';
-export { default as Modal } from './Modal';
