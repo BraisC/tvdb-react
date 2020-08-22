2427cf5b82fcef61bcb9af89b89ba1cb7b1f0aa2
Brais <braiscao.dev@gmail.com>
2020-08-22 20:29:46 +0200

    👀 item Content loader

--- src/components/ShowList/ShowItem/ContentLoader/ContentLoader.jsx
+++ src/components/ShowList/ShowItem/ContentLoader/ContentLoader.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Styled } from './styled';
 
-const ContentLoader = (props) => (
-  <Styled.ShowItemContentLoader>Loading Content</Styled.ShowItemContentLoader>
+const ContentLoader = () => (
+  <Styled.ShowItemContentLoader>
+    <Styled.Placeholder height={6} />
+    <Styled.Placeholder height={10} />
+    <Styled.Placeholder height={4} />
+    <Styled.Placeholder height={4} />
+    <Styled.Placeholder height={4} />
+  </Styled.ShowItemContentLoader>
 );
 
 export default ContentLoader;
--- src/components/ShowList/ShowItem/ContentLoader/styled.js
+++ src/components/ShowList/ShowItem/ContentLoader/styled.js
@@ -2,8 +2,19 @@ import styled from 'styled-components';
 
 const ShowItemContentLoader = styled.div`
   transform: scaleX(0.87);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+`;
+
+const Placeholder = styled.div`
+  width: 100%;
+  background-color: var(--color-primary);
+  height: ${(props) => props.height + 'rem'};
 `;
 
 export const Styled = {
   ShowItemContentLoader,
+  Placeholder,
 };
