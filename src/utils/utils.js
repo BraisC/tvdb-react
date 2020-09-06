629de308d87da97340cbc88ad03b5c69a6a708c1
Brais <braiscao.dev@gmail.com>
2020-09-06 20:03:14 +0200

    🧪 Finished tests of limitTextLength and refactor

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,21 +9,19 @@ const getCountryName = (countryCode) => {
 
 const generateTitle = (show) => (show.year ? `${show.name} (${show.year})` : show.name);
 
-const limitTextLength = (title, limit = 18) => {
+const limitTextLength = (title, limit = 20) => {
   const newTitle = [];
 
   if (title.length > limit) {
     title.split(' ').reduce((accumulator, currentValue) => {
-      if (accumulator + currentValue.length < limit) {
+      if (accumulator + currentValue.length + 1 <= limit) {
         newTitle.push(currentValue);
+        newTitle.push(' ');
       }
-      return accumulator + currentValue.length;
+      return accumulator + currentValue.length + 1;
     }, 0);
 
-    if (newTitle.join(' ').length === title.length) {
-      return newTitle.join(' ');
-    }
-    return `${newTitle.join(' ')} ...`;
+    return `${newTitle.join('')}...`;
   }
 
   return title;
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -11,15 +11,19 @@ describe('getCountryName', () => {
   test('if gets country name', () => {
     expect(utils.getCountryName('US')).toBe('United States');
     expect(utils.getCountryName('ES')).toBe('Spain');
+    expect(utils.getCountryName('KK')).toBe('KK');
   });
 });
 
 const longString =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec faucibus sapien. Nullam molestie luctus quam sed aliquam. Mauris eget sodales tortor. In aliquam ut nulla quis luctus. Nulla in porta ligula, at posuere urna. Aliquam sed fringilla lectus. Fusce consectetur interdum rutrum. Donec nec vestibulum justo. Suspendisse risus ligula, vestibulum quis feugiat ut, hendrerit quis ipsum. Proin turpis arcu, rhoncus sit amet lacus eu, accumsan accumsan odio. Morbi ultrices vehicula ex, vitae semper massa rhoncus vitae. Duis ac porttitor sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum consequat nulla quis purus laoreet, vulputate auctor nulla fermentum. Nulla facilisi. Sed magna lacus, pharetra eget felis non, volutpat interdum tellus. Vivamus tincidunt aliquet efficitur. Ut non lorem fermentum, commodo risus vel, semper turpis. Etiam eget arcu ut ante bibendum vestibulum in bibendum ligula. Praesent commodo orci enim, a imperdiet sapien blandit in. Etiam vitae.';
 
+const shortString = 'Hello my dear';
+
 describe('limitTextLength', () => {
   test('if limits length', () => {
-    expect(utils.limitTextLength(longString).length).toBeLessThanOrEqual(21);
-    expect(utils.limitTextLength(longString, 22).length).toBeLessThanOrEqual(25);
+    expect(utils.limitTextLength(longString, 20).length).toBeLessThanOrEqual(23);
+    expect(utils.limitTextLength(longString, 30).length).toBeLessThanOrEqual(33);
+    expect(utils.limitTextLength(shortString)).toHaveLength(shortString.length);
   });
 });
